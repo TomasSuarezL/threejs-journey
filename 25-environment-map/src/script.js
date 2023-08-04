ac36d1eb2dde60db855f4ac6c77e93f5aa2989dc
Tomas Suarez Lissi <tomas.sl@hotmail.com>
2023-08-04 12:31:55 +1000

    lesson 25 complete

--- 25-environment-map/src/script.js
+++ 25-environment-map/src/script.js
@@ -1,91 +1,190 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+import { GroundProjectedSkybox } from "three/examples/jsm/objects/GroundProjectedSkybox.js";
+import * as dat from "lil-gui";
 
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI();
+const global = {
+  envMapIntensity: 1,
+};
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
+
+const updateAllMaterials = () => {
+  scene.traverse((child) => {
+    if (child.isMesh && child.material.isMeshStandardMaterial) {
+      child.material.envMapIntensity = global.envMapIntensity;
+    }
+  });
+};
+
+// Loaders
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", (gltf) => {
+  gltf.scene.scale.set(10, 10, 10);
+  scene.add(gltf.scene);
+
+  updateAllMaterials();
+});
+
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const rgbeLoader = new RGBELoader();
+const textureLoader = new THREE.TextureLoader();
+
+// const environmentMap = cubeTextureLoader.load([
+//   "/environmentMaps/0/px.png",
+//   "/environmentMaps/0/nx.png",
+//   "/environmentMaps/0/py.png",
+//   "/environmentMaps/0/ny.png",
+//   "/environmentMaps/0/pz.png",
+//   "/environmentMaps/0/nz.png",
+// ]);
+
+// scene.environment = environmentMap;
+// scene.background = environmentMap;
+
+// rgbeLoader.load("/environmentMaps/blender-2k.hdr", (environmentMap) => {
+//   environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+//   // scene.background = environmentMap;
+//   scene.environment = environmentMap;
+// });
+
+const environmentMap = textureLoader.load(
+  "/environmentMaps/blockadesLabsSkybox/interior_views_cozy_wood_cabin_with_cauldron_and_p.jpg"
+);
+environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+environmentMap.colorSpace = THREE.SRGBColorSpace;
+scene.background = environmentMap;
+
+scene.backgroundBlurriness = 0;
+scene.backgroundIntensity = 1;
+
+const holyDonut = new THREE.Mesh(
+  new THREE.TorusGeometry(16, 0.5),
+  new THREE.MeshBasicMaterial({ color: new THREE.Color(10, 4, 2) })
+);
+holyDonut.position.y = 3.5;
+scene.add(holyDonut);
+holyDonut.layers.enable(1);
+
+const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(256, {
+  type: THREE.HalfFloatType,
+});
+
+scene.environment = cubeRenderTarget.texture;
+
+// rgbeLoader.load("/environmentMaps/2/2k.hdr", (environmentMap) => {
+//   environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+//   scene.environment = environmentMap;
+
+//   const skybox = new GroundProjectedSkybox(environmentMap);
+//   skybox.scale.setScalar(50);
+//   scene.add(skybox);
+
+//   gui.add(skybox, "radius", 1, 200, 0.1);
+//   gui.add(skybox, "height", 1, 200, 0.1);
+// });
+
+gui.add(global, "envMapIntensity").min(0).max(10).step(0.01).onFinishChange(updateAllMaterials);
+gui.add(scene, "backgroundBlurriness").min(0).max(1).step(0.01);
+gui.add(scene, "backgroundIntensity").min(0).max(10).step(0.01);
 
 /**
  * Torus Knot
  */
 const torusKnot = new THREE.Mesh(
-    new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
-    new THREE.MeshBasicMaterial()
-)
-torusKnot.position.y = 4
-scene.add(torusKnot)
+  new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
+  new THREE.MeshStandardMaterial({ roughness: 0.03, metalness: 1, color: 0xaaaaaa })
+);
+torusKnot.position.x = -4;
+torusKnot.position.y = 4;
+scene.add(torusKnot);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(4, 5, 4)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.set(4, 5, 4);
+scene.add(camera);
+
+const cubeCamera = new THREE.CubeCamera(0.1, 100, cubeRenderTarget);
+cubeCamera.layers.set(1);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.target.y = 3.5
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.target.y = 3.5;
+controls.enableDamping = true;
+
+// Lights
+
+// const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+// scene.add(ambientLight);
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-const tick = () =>
-{
-    // Time
-    const elapsedTime = clock.getElapsedTime()
+const clock = new THREE.Clock();
+const tick = () => {
+  // Time
+  const elapsedTime = clock.getElapsedTime();
 
-    // Update controls
-    controls.update()
+  if (holyDonut) {
+    holyDonut.rotation.x = Math.sin(elapsedTime) * 2;
 
-    // Render
-    renderer.render(scene, camera)
+    cubeCamera.update(renderer, scene);
+  }
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Update controls
+  controls.update();
 
-tick()
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
