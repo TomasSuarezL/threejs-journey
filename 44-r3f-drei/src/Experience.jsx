86dba5c63062977503f37b7a1f787013b539ef13
Tomas Suarez Lissi <tomas.sl@hotmail.com>
2023-09-01 18:48:40 +1000

    lesson 44 completed

---
+++ 44-r3f-drei/src/Experience.jsx
@@ -0,0 +1,76 @@
+import { useFrame } from "@react-three/fiber";
+import {
+  OrbitControls,
+  TransformControls,
+  PivotControls,
+  Html,
+  Text,
+  Float,
+  MeshReflectorMaterial,
+} from "@react-three/drei";
+import { useRef } from "react";
+
+export default function Experience() {
+  const cube = useRef();
+  const sphere = useRef();
+
+  return (
+    <>
+      <OrbitControls makeDefault />
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <PivotControls
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        lineWidth={4}
+        axisColors={["#9381ff", "#ff4d6d", "#7ae582"]}
+        scale={200}
+        fixed={true}
+      >
+        <mesh ref={sphere} position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+          <Html
+            position={[1, 1, 0]}
+            wrapperClass="label"
+            center
+            distanceFactor={8}
+            occlude={[sphere, cube]}
+          >
+            That's a sphere 👍
+          </Html>
+        </mesh>
+      </PivotControls>
+
+      <mesh ref={cube} position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+      <TransformControls object={cube} />
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <MeshReflectorMaterial
+          resolution={512}
+          blur={[512, 512]}
+          mixBlur={0.8}
+          mirror={0.2}
+          color="greenyellow"
+        />
+      </mesh>
+
+      <Float speed={5} floatIntensity={4}>
+        <Text
+          font="./bangers-v20-latin-regular.woff"
+          fontSize={1}
+          position-y={2}
+          textAlign="center"
+          maxWidth={2}
+        >
+          I Love R3F
+        </Text>
+      </Float>
+    </>
+  );
+}
