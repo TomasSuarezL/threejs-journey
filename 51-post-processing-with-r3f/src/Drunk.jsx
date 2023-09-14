f8d62feead6caa948f739abd5c31c61f44eca702
Tomas Suarez Lissi <tomas.sl@hotmail.com>
2023-09-14 16:21:51 +1000

    lesson 51 completed

---
+++ 51-post-processing-with-r3f/src/Drunk.jsx
@@ -0,0 +1,8 @@
+import { forwardRef } from "react";
+import DrunkEffect from "./DrunkEffect";
+
+export const Drunk = forwardRef((props, ref) => {
+  const effect = new DrunkEffect(props);
+
+  return <primitive object={effect} ref={ref} />;
+});
